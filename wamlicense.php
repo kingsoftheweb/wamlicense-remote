02ee19ca9add09238be10cff833993de97d1396d
Mazen <kingsofthewebofficial@gmail.com>
2022-06-17 17:41:14 +0200

    [AJK-140-fix-2] Fixed issue with parent order and its related subscriptions

--- wamlicense.php
+++ wamlicense.php
@@ -3,7 +3,7 @@
  * Plugin Name: WAM License
  * Plugin URI:
  * Description: Generates XML license for WAM products.
- * Version: 1.1.1
+ * Version: 1.2
  * Author: AJ Tek Corporation
  * Author URI:
  * Text Domain: wamlicense
